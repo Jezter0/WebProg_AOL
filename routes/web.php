489c737ec0de868234205b117444518ac3f8b042
sopochles <richaelsuhanda@gmail.com>
2025-12-14 03:30:48 +0700

    Add budget feature

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\BudgetController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -27,4 +28,5 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::resource('categories', CategoryController::class);
     Route::resource('transactions', TransactionController::class)->except(['show']);
+    Route::resource('budgets', BudgetController::class);
 });
